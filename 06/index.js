9066e4e05a27001060694d61c7757ff3f480a53d
Chinmay <chinz.sandur07@gmail.com>
2023-12-14 15:50:29 +0530

    add solution to Q2

--- 06/index.js
+++ 06/index.js
@@ -1,7 +1,26 @@
 //Q1
 let age = alert("what is your age");
-if(age > 10 && age <=20){
+if (age > 10 && age <= 20) {
     console.log("Print Success");
-}else{
+} else {
     console.log("Failed")
+}
+
+//Q2
+
+let age = 18;
+switch (age) {
+    case 12:
+        console.log("your age is 12");
+        break;
+    case 13:
+        console.log("Your age is 13");
+        break;
+    case 14:
+        console.log("Your age is 14");
+        break;
+    case 18:
+        console.log("Your age is 18")
+        break;
+    default: console.log("your age is not special");
 }
