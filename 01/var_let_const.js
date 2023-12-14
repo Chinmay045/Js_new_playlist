5c0067cd3faef69a30f985530560f12b3ef31f59
Chinmay <chinz.sandur07@gmail.com>
2023-12-14 09:44:29 +0530

    add example of const

--- 01/var_let_const.js
+++ 01/var_let_const.js
@@ -1,13 +1,16 @@
-console.log("JS tutorial 3");
-let a = 45;
-a = 43;
-console.log(a)
-var b = "Chinmay";
-console.log(b);
+// console.log("JS tutorial 3");
+// let a = 45;
+// a = 43;
+// console.log(a)
+// var b = "Chinmay";
+// console.log(b);
 
-{
-    var b = "Chinz";
-    console.log(b); 
-}
+// {
+//     var b = "Chinz";
+//     console.log(b); 
+// }
 
-console.log(b)
+// console.log(b)
+
+const a = "45";
+console.log(a);
