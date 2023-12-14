00d2e64affefb8b6642f52bd8268ad9637ff3ff7
Chinmay <chinz.sandur07@gmail.com>
2023-12-14 12:16:10 +0530

    add solution to Q3

--- 03/practice_1.js
+++ 03/practice_1.js
@@ -5,4 +5,17 @@ let num = 5;
 console.log(name + num);
 
 //Q2
-console.log(typeof (name), typeof(num));
+console.log(typeof (name), typeof (num));
+
+//Q3
+const a1= {
+    name: "chinmay",
+    section: 1,
+
+}
+
+//Q4
+a1["friend"] = "Shubham",
+a1["name"] = "Lovish"
+console.log(a1)
+
