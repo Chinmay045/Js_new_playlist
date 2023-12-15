19ab6b716fc2f578a7f2de6e68aaf436f6c91a91
Chinmay <chinz.sandur07@gmail.com>
2023-12-15 16:59:21 +0530

    add solution to Q3

--- 09/ps.js
+++ 09/ps.js
@@ -12,3 +12,9 @@ let marks = {
 for(let key in marks){
     console.log("The marks of " + key + " are " + marks[key])
 }
+
+const mean = (a, b, c, d ) =>{
+    return (a + b + c + d)/4;
+}
+
+console.log(mean(2,4,6,7));
