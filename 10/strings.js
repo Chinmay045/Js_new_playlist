4cec58a4e0baed96c287dc43028a59549d53dd91
Chinmay <chinz.sandur07@gmail.com>
2023-12-15 17:08:21 +0530

    add example for template literals

--- 10/strings.js
+++ 10/strings.js
@@ -2,3 +2,10 @@ let name = "Chinmay";
 console.log(name);
 let name = 'Chinmay'
 console.log(name)
+console.log(name[0]);
+console.log(name[1]);
+
+let f1 = "pramod";
+let f2 = "Server";
+let friend = `${f1} is  a friend of ${f2} !!`
+console.log(friend)
