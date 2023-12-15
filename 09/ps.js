d869934b321c394487cff16fc83f1e8b258baea4
Chinmay <chinz.sandur07@gmail.com>
2023-12-15 16:52:37 +0530

    add solution to Q1

---
+++ 09/ps.js
@@ -0,0 +1,10 @@
+let marks = {
+    harry: 90,
+    Shubham: 89,
+    lovish: 90,
+    Monika: 4
+}
+
+for (let i = 0; i < Object.keys(marks).length; i++) {
+    console.log("Marks " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+}
