fde4c27c2175171b347016502d78d01a8740207b
Chinmay <chinz.sandur07@gmail.com>
2023-12-16 11:05:43 +0530

    add example for slice

--- 12/index.js
+++ 12/index.js
@@ -2,3 +2,7 @@ let name = "fox";
 let word = "A quick brown fox";
 console.log(word.includes("fox"));
 console.log(word.startsWith("fox"));
+
+let str = "please give me Rs.1000";
+let amount = Number.parseInt(str.slice(15));
+console.log( typeof amount);
