be73c252b2f30d5b4e7fc0076b7942f861990f9d
Chinmay <chinz.sandur07@gmail.com>
2023-12-16 11:09:22 +0530

    add example for string immutability

--- 12/index.js
+++ 12/index.js
@@ -5,4 +5,7 @@ console.log(word.startsWith("fox"));
 
 let str = "please give me Rs.1000";
 let amount = Number.parseInt(str.slice(15));
+str[0] = "c";
+str = "Please"
+console.log(str)
 console.log( typeof amount);
