2c2c633e964101bdbb03fb51fb5c5dccd3e50021
Chinmay <chinz.sandur07@gmail.com>
2023-12-16 16:57:49 +0530

    add example for push

--- 14/array_methods.js
+++ 14/array_methods.js
@@ -1,4 +1,4 @@
-let num = [1, 2, 3, 4, 5];
+let num = [1, 2, 6, 4, 5];
 let b = num.toString();
 console.log(b);
 let c = num.join("-");
@@ -6,4 +6,9 @@ console.log(c ,typeof c);
 console.log(num, typeof num);
 let r = num.pop();
 console.log(num, r);
+let s = num.pop();
+console.log(num, s);
+let e = num.push(50);
+console.log(num, e);
+
 // console.log()
