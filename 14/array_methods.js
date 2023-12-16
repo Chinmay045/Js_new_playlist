1e83f76ccc9753d0b1accf3e84f9dc11aff00150
Chinmay <chinz.sandur07@gmail.com>
2023-12-16 16:54:41 +0530

    add example for pop

--- 14/array_methods.js
+++ 14/array_methods.js
@@ -2,4 +2,8 @@ let num = [1, 2, 3, 4, 5];
 let b = num.toString();
 console.log(b);
 let c = num.join("-");
-console.log(c); 
+console.log(c ,typeof c);
+console.log(num, typeof num);
+let r = num.pop();
+console.log(num, r);
+// console.log()
