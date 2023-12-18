24e4311557dea7f094505a58aa2ca0eff7897e43
Chinmay <chinz.sandur07@gmail.com>
2023-12-18 09:24:49 +0530

    add example for concat method

--- 15/array_methods.js
+++ 15/array_methods.js
@@ -1,3 +1,5 @@
-let num = [1, 2, 3, 4, 5, 6, 8, 9, 3];
-let del = delete num[0];
-console.log(num);
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// delete num[3];
+// console.log(num);
+let num_more = [10, 11, 12, 13, 14, 15, 16, 17];
+console.log(num.concat(num_more));
