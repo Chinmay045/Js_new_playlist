10ab76c4573f9317f63ffe168be6d289b831fc29
Chinmay <chinz.sandur07@gmail.com>
2023-12-18 09:31:32 +0530

    add example for sort method

--- 15/array_methods.js
+++ 15/array_methods.js
@@ -1,5 +1,6 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let num = [ 6, 7, 8, 91, 2, 3, 4, 5];
 // delete num[3];
 // console.log(num);
 let num_more = [10, 11, 12, 13, 14, 15, 16, 17];
 console.log(num.concat(num_more));
+console.log(num.sort());
