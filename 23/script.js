9cdbc101dc7e5d9a438e24dd4804a781164cfca3
Chinmay <chinz.sandur07@gmail.com>
2023-12-27 10:52:17 +0530

    add color to each individual elements

--- 23/script.js
+++ 23/script.js
@@ -1,3 +1,6 @@
 let navb = document.getElementsByClassName("nav");
 navb[0].firstElementChild.style.color = "red";
 
+Array.from(document.getElementsByTagName("li")).forEach(element => {
+    element.style.background = "yellow";
+});
