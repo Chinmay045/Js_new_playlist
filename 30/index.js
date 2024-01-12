a86d818dd9f6ac5c742823a65fabd11d8b5f15a5
Chinmay <chinz.sandur07@gmail.com>
2024-01-12 10:16:54 +0530

    add example to setInterval()

--- 30/index.js
+++ 30/index.js
@@ -1,3 +1,6 @@
-setTimeout(function(){
-    console.log("Hi!");
-},2000);
+// setTimeout(function(){
+//     console.log("Hi!");
+// },2000);
+setInterval(function(){
+    console.log("run after every 2 seconds");
+},2000)
