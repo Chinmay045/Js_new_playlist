4b7ecc0776633b58c10dd21e2a9d340b40d018ef
Chinmay <chinz.sandur07@gmail.com>
2024-01-12 10:56:37 +0530

    add example for event handler

--- 30/index.js
+++ 30/index.js
@@ -1,6 +1,16 @@
 // setTimeout(function(){
 //     console.log("Hi!");
 // },2000);
-setInterval(function(){
-    console.log("run after every 2 seconds");
-},2000)
+
+// let timer = setInterval(function () {
+//     console.log("run after every 2 seconds");
+// }, 2000);
+
+var butn = document.getElementById('btn');
+butn.addEventListener('click', function (e) {
+    console.log("hi macha")
+    console.log(butn.id);
+    console.log(e.target);
+})
+
+
