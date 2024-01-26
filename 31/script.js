d648ba9b4bfac21bfa9e327aea32dda43d2acf77
Chinmay <chinz.sandur07@gmail.com>
2024-01-26 16:17:17 +0530

    add example for promise

--- 31/script.js
+++ 31/script.js
@@ -2,9 +2,25 @@ let promise = new Promise((resolve, reject) => {
     console.log("promise is pending");
     setTimeout(() => {
         console.log('it will resolve after 2')
-        reject(new Error ("error occured"))
+        reject(new Error("error occured"))
         // resolve(true);
     }, 2000)
 })
 
-console.log(promise)
+
+let promise1 = new Promise((resolve, reject) => {
+    console.log("promise is pending");
+    setTimeout(() => {
+        // console.log('it will resolve after 2')
+        resolve(true)
+        // resolve(true);
+    }, 2000)
+})
+promise.then((value) => {
+    console.log(value);
+}).catch((err) => {
+    console.log('error occured' + err)
+})
+promise1.then((value) => {
+    console.log(value)
+})
