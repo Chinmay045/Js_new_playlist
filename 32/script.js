9e68daa85ea7770423e5cf3b81610ecbadd5d793
Chinmay <chinz.sandur07@gmail.com>
2024-01-27 10:57:33 +0530

    add example for promis api

--- 32/script.js
+++ 32/script.js
@@ -1,17 +1,24 @@
-let p1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('resolved after 2 seconds')
-        resolve(56);
-    }, 2000)
-})
+// let p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('resolved after 2 seconds')
+//         resolve(56);
+//     }, 2000)
+// })
 
-p1.then((value) => {
-    // console.log(value);
-    let p2 = new Promise((resolve, reject) => {
-        resolve('promise 2');
-    })
-    return p2;
-}).then((value) => {
-    console.log(value)
-    // console.log('we are done');
-})
+// p1.then((value) => {
+//     // console.log(value);
+//     let p2 = new Promise((resolve, reject) => {
+//         resolve('promise 2');
+//     })
+//     return p2;
+// }).then((value) => {
+//     console.log(value)
+//     // console.log('we are done');
+// })
+
+// p1.then(()=>{
+//     console.log('part 2')
+// })
+
+let promise = Promise.resolve(5);
+console.log(promise)
