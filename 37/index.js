4093d0d1043c20150a368b95c226a83f80b01e60
Chinmay <chinz.sandur07@gmail.com>
2024-01-31 15:43:51 +0530

    add example for calss and objects

---
+++ 37/index.js
@@ -0,0 +1,12 @@
+class RailwayForm {
+    submit() {
+        alert('form submitted');
+    }
+
+    cancel() {
+        alert('form is cancelled');
+    }
+}
+
+let chinz = new RailwayForm()
+chinz.submit();
