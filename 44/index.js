31441366f3e47320b0343f12fde0aab7ac1a42cd
Chinmay <chinz.sandur07@gmail.com>
2024-02-01 16:33:12 +0530

    add example for destructuring

--- 44/index.js
+++ 44/index.js
@@ -6,4 +6,12 @@
 
 const obj = { a: 1, b: 2 };
 const { a, b } = obj;
-console.log(a,b)
+console.log(a, b)
+
+
+let arr1 = [2, 3, 5, 6];
+function sum(v1, v2, v3) {
+    return v1 + v2 + v3;
+}
+
+console.log(sum(...arr1));
