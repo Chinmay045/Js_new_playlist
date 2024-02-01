4449e385774e3086b05a51a18d39c3198a76ac05
Chinmay <chinz.sandur07@gmail.com>
2024-02-01 12:38:41 +0530

    add practice set problem 2

--- 42/index.js
+++ 42/index.js
@@ -1,8 +1,30 @@
-class Complex {
-    constructor(real, imaginary) {
-        this.real = real;
-        this.imaginary = imaginary
+// class Complex {
+//     constructor(real, imaginary) {
+//         this.real = real;
+//         this.imaginary = imaginary
+//     }
+// }
+
+// let a = new Complex(2,4);
+
+class Human {
+    constructor(name, favfood) {
+        this.name = name;
+        this.favfood = favfood;
+    }
+
+    walk() {
+        console.log(this.name + 'human is walking')
     }
 }
 
-let a = new Complex(2,4);
+class student extends Human {
+
+    walk() {
+        console.log(this.name + ' Student is walking')
+    }
+}
+
+let o = new student('chinmay');
+o.walk();
+console.log(o instanceof Human)
